0a83e92a4df1e4c54db49facc68cb7157d988acd
Nabil-Aditya <nabiladitya2203@gmail.com>
2024-10-20 23:12:55 +0700

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -13,16 +13,99 @@
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+    <!-- SweetAlert2 CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            max-width: 100%;
+            padding: 0 15px;
+        }
+
+        .login-image {
+            width: 70%;
+            height: auto;
+            margin-top: 5%;
+            margin-left: 15%;
+            padding: 20px;
+        }
+
+        @media (max-width: 768px) {
+            .login-image {
+                padding: 10px;
+            }
+        }
+
+        @media (max-width: 576px) {
+            .login-image {
+                padding: 5px;
+            }
+        }
+
+        .img-fluid {
+            margin-top: 5%;
+            width: 15%;
+            /* Sesuaikan persentase sesuai kebutuhan */
+            border-radius: 8px;
+            margin-left: 14%;
+        }
+
+        .bold-black-text {
+            color: black;
+            font-weight: bold;
+        }
+
+        .custom-link {
+            color: rgb(25, 25, 112);
+            text-decoration: none;
+            font-size: 13px;
+        }
+
+        .custom-link:hover {
+            text-decoration: underline;
+        }
+
+        .custom-button {
+            background-color: rgb(25, 25, 112);
+            border-color: rgb(25, 25, 112);
+        }
+
+        .custom-button:hover {
+            background-color: rgb(0, 40, 199);
+            border-color: rgb(0, 0, 139);
+        }
+
+        .custom-heading {
+            color: rgb(25, 25, 112);
+            font-weight: bold;
+            font-size: 25px;
+        }
+
+        .card.custom-shadow {
+            box-shadow: 0 12px 24px rgba(0, 0, 0, 0.5);
+        }
+
+        .small-text {
+            font-size: 12px;
+        }
+    </style>
+
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-warning">
 
     <div class="container">
 
@@ -31,31 +114,40 @@
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card o-hidden border-0 custom-shadow my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <img src="./img/sipelita.jpg" alt="Logo" class="img-fluid"> V.1.0
+                                <img src="./img/forgot_password.jpg" alt="Forgot Password Image" class="login-image">
+                                <div class="text-center mt-1">
+                                    <a href="#" class="btn btn-link custom-link">Buku Panduan</a> |
+                                    <a href="#" class="btn btn-link custom-link">Hubungi Kami</a> |
+                                    <a href="#" class="btn btn-link custom-link">Tim Pengembang</a>
+                                </div>
+                                <div class="text-center mt-2">
+                                    <p class="small-text">Hak Cipta @Sipelita 2024</p>
+                                </div>
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                        <p class="mb-4">We get it, stuff happens. Just enter your email address below
-                                            and we'll send you a link to reset your password!</p>
+                                        <h1 class="h4 mb-2 custom-heading">Lupa Kata Sandi?</h1>
+                                        <p class="mb-5">Jangan khawatir, cukup masukkan alamat email kamu di bawah dan kami akan mengirimkan email untuk mereset kata sandi!</p>
                                     </div>
-                                    <form class="user">
+                                    <form id="forgotPasswordForm" class="user" method="post" action="forgot-password-handler.php">
+                                        <span class="bold-black-text">Email Anda</span>
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukkan Alamat Email...">
                                         </div>
-                                        <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                            Reset Password
-                                        </a>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block custom-button">
+                                            Reset Kata Sandi
+                                        </button>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="login.php">Kembali Masuk?</a>
+                                        <a class="custom-link" href="login.php">Kembali Masuk?</a>
                                     </div>
                                 </div>
                             </div>
@@ -78,6 +170,25 @@
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- SweetAlert handling -->
+    <script>
+        document.getElementById("forgotPasswordForm").addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevent default form submission
+            Swal.fire({
+                icon: "success",
+                title: "Permintaan Reset Berhasil!",
+                text: "Email reset kata sandi telah dikirim ke email Anda. Harap Tunggu Beberapa Saat!",
+                confirmButtonText: "Saya Mengerti",
+                position: "center", // Center the alert
+                allowOutsideClick: false // Prevent closing the alert by clicking outside
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.close(); // Just close the alert on "Saya Mengerti"
+                }
+            });
+        });
+    </script>
 
 </body>
 
