6c246b92ef392db22526c6fc5471aa8ac0cfae78
Nabil-Aditya <nabiladitya2203@gmail.com>
2024-12-11 18:53:53 +0700

    Update prodi.php

--- admin/prodi.php
+++ admin/prodi.php
@@ -65,7 +65,7 @@ if ($currentHour >= 0 && $currentHour < 12) {
         rel="stylesheet">
 
     <!-- styles this template -->
-    <link href="../css/sb-admin-2.minn.css" rel="stylesheet">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="../css/admin/index-admin.css" rel="stylesheet">
 
